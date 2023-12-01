3442da31ba142955ea7d41eb3eb1c36fd1ad47db
Rofi Nugraha <rofinugrahaputra@gmail.com>
2023-12-01 13:26:18 +0700

    fixing code

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
 const Inert = require('@hapi/inert');
-const path = require('path');
+// const path = require('path');
 
 // Users
 const users = require('./api/users');
@@ -35,6 +35,9 @@ const ExportsValidator = require('./validator/exports');
 // Uploads
 const uploads = require('./api/uploads');
 
+// Caches
+const CacheService = require('./services/redis/CacheService');
+
 // Implementasi Local Storage
 // const StorageService = require('./services/storage/StorageService');
 
@@ -42,8 +45,9 @@ const StorageService = require('./services/S3/StorageService'); // Implementasi 
 const UploadsValidator = require('./validator/uploads');
 
 const init = async () => {
-  const collaborationsService = new CollaborationsService();
-  const notesService = new NotesService(collaborationsService);
+  const cacheService = new CacheService();
+  const collaborationsService = new CollaborationsService(cacheService);
+  const notesService = new NotesService(collaborationsService, cacheService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
 
